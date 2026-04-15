482bb2fd493a1987d6b249911d90ae89972ca3ec
Amel <amelghanem25@gmail.com>
2026-04-15 16:31:36 +0200

    Personnalisation du formulaire (rajout elements)

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use App\form\RegisterUserType;
+use App\Form\RegisterUserType;
 
 final class RegisterController extends AbstractController
 {
--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -7,19 +7,52 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class RegisterUserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
-            ->add('firstName')
-            ->add('lastName')
+            ->add('email',EmailType::class , [
+                'label'=> "Votre adresse mail",
+                'attr'=>[
+                    'placeholder' => 'Indiquez votre Adresse email',
+                    'class' => 'form-control w-75',
+                ]
+               
+
+            ])
+            ->add('password', PasswordType::class,[
+                'label'=> "Votre mot de passe",
+                'attr'=>[
+                    'placeholder' => 'Indiquez votre Mot de passe',
+                    'class' => 'form-control w-75',
+                ]
+            ])
+            ->add('firstName', TextType::class,[
+                'label'=> "Votre nom",
+                'attr'=>[
+                    'placeholder' => 'Indiquez votre nom',
+                    'class' => 'form-control w-75',
+                ]
+            ])
+            ->add('lastName', TextType::class,[
+                'label'=> "Votre prénom",
+                'attr'=>[
+                    'placeholder' => 'Indiquez votre nom',
+                    'class' => 'form-control w-75',
+                ]
+            ])
             //  j'ai ajouté le btn submi avec add puis j'ai pris le lien en haut sur la doc 
-            ->add('Submit', SubmitType::class)
+            ->add('Submit', SubmitType::class,[
+                'label'=> "Valider",
+                'attr'=>[
+                    'class'=> "btn btn-success"
+                ]
+            ])
         ;
     }
 
