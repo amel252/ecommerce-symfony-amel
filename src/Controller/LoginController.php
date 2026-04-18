fd98301f3cf08722f14f363f85fa98712f03e85b
Amel <amelghanem25@gmail.com>
2026-04-18 15:02:10 +0200

    Création de formulaire de connexion et la route logout

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+final class LoginController extends AbstractController
+{
+    #[Route('/connexion', name: 'app_login')]
+    public function index(AuthenticationUtils  $authenticationUtils): Response
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+        
+
+        return $this->render('login/index.html.twig',[
+            'last_username' => $lastUsername,
+            'error'=> $error,
+        ]);
+            
+    }
+    //  création de l'api logout
+    #[Route('/deconnexion', name: 'app_logout', methods:['GET'])]
+    public function logout(): void
+    {
+       throw new \logicException('This method can be blanck - it will be intercepted by logout key on you firewall');
+        
+            
+    }
+}
